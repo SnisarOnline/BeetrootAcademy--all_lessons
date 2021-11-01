060bb0ce97a908da8364a1bb0681c534bad6db36
John Doe <snisa.online@gmail.com>
2021-11-01 19:45:02 +0200

    updated #25

---
+++ 25-Introducing_JS/README.md
@@ -0,0 +1,23 @@
+﻿# Introducing_JS
+> Знакомство с JS
+
+
+## Lesson plan:
++ Что т-е LiveScript
++ Что т-е ECMAScript [262](https://www.ecma-international.org/publications-and-standards/standards/ecma-262/)
++ [Браузерное окружение](https://learn.javascript.ru/browser-environment)
++ Способы подключение js
++ Восемь основных типов данных
++ Сравнение данных
+
+
+## Useful links:
++ [Классификация языков программирования](https://studme.org/97344/informatika/klassifikatsiya_yazykov_programmirovaniya)
++ [Основы JavaScript](https://learn.javascript.ru/first-steps)
++ [Строгий режим — "use strict"](https://learn.javascript.ru/strict-mode#use-strict)
++ [Браузерное окружение](https://learn.javascript.ru/browser-environment)
++ [DOM-дерево](https://learn.javascript.ru/dom-nodes)
++ []()
+
+###### Youtube
++ [JavaScript Document object model (DOM)](https://youtu.be/DuWyc76lYC4)
---
+++ 25-Introducing_JS/example/app.js
@@ -0,0 +1,86 @@
+"use strict";
+
+alert('Hello world! from app.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ================================================================== */
+/* ===================== Браузерное окружение  ====================== */
+/* ================================================================== */
+// console.log('window', window);
+
+
+/* ============= 1) BOM ============= */
+/*
+console.log('navigator', navigator);
+console.log('screen', screen);
+console.log('location', location);
+console.log('location.href', location.href);
+console.log('frames', frames);
+console.log('history', history);
+*/
+
+/* ============= 2) DOM ============= */
+// console.log('document', document);
+
+/*
+console.log('document.head', document.head);
+console.log('document.body', document.body);
+*/
+
+
+/* ============= 3) JavaScript ============= */
+/*
+const promptResult = prompt("Кто ты ?");
+console.log('promptResult :', promptResult);
+*/
+
+
+
+
+
+
+/* ================================================================== */
+/* =====================  Типы данных =============================== */
+/* ================================================================== */
+/*
+boolean, string, (number, BigInt), object, symbol, undefined, null
+*/
+
+// let a = 5;
+// const a = 5;
+
+
+// console.log('---------- Елементарные Типы Данных ----------');
+// console.log(typeof true)
+// console.log(typeof 'Javascript') // "", ''
+// console.log(typeof `Javascript and ${10 + 5}`) // ``
+// console.log(typeof 0)
+// console.log(typeof 10n)
+// console.log(typeof BigInt("1") ) // BigInt(1) / 1n
+// console.log(typeof NaN) // Специальные значения
+// console.log(typeof Infinity) // Специальные значения
+// console.log(typeof {})
+// console.log(typeof Math)
+// console.log(typeof Symbol('JS'))
+// console.log(typeof undefined)
+// исключения
+// console.log(typeof null) //
+// console.log(typeof [])
+// console.log(typeof function() {})
